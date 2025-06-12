ce70030d539d2e44686d6c27b4ed25e900e879c5
Gabriel C. Trahan <gabrieltrahan777@hotmail.com>
2025-06-12 15:42:32 -0500

    Implement initial setup utility

---
+++ setup.py
@@ -0,0 +1,24 @@
+"""# lucidium.setup
+
+# Lucidium setup utility.
+"""
+
+from setuptools import find_packages, setup
+
+setup(
+    name =              "lucidium",
+    version =           "0.0.0",
+    author =            "Gabriel C. Trahan",
+    author_email =      "gabrieltrahan777@hotmail.com",
+    description =       """Experiments in neuro-symbolic reinforcement learning in the pursuit of 
+                        developing agents/methods to aid in organic reasoning, inference, and 
+                        logic.""",
+    license =           "GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007",
+    license_files =     ("LICENSE"),
+    url =               "https://github.com/theokoles7/lucidium",
+    packages =          find_packages(),
+    python_requires =   ">=3.10",
+    install_requires =  [
+    
+                        ]
+)
