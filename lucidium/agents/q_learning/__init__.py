e88292b7fe7135f2e12b113859c81767b1dfcca2
Gabriel C. Trahan <gabrieltrahan777@hotmail.com>
2025-08-03 16:46:35 -0500

    Implement `Q-Learning` agent

---
+++ lucidium/agents/q_learning/__init__.py
@@ -0,0 +1,18 @@
+"""# ludorum.agents.q_learning
+
+This package implements the Q-Learning agent proposed in the 1992 paper by Watkins & Dayan.
+"""
+
+__all__ =   [
+                # Agent class.
+                "QLearning",
+                
+                # Main process.
+                "main"
+            ]
+
+# Agent class.
+from agents.q_learning.__base__ import QLearning
+
+# Main process.
+from agents.q_learning.__main__ import main
