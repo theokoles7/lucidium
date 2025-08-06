74f3dcb28de0c1f8aa5f074be244ce3f7e1b4d2b
Gabriel C. Trahan <gabrieltrahan777@hotmail.com>
2025-08-06 12:33:32 -0500

    Update `lucidium.environments.block_world` package

---
+++ lucidium/environments/block_world/__init__.py
@@ -0,0 +1,12 @@
+"""# lucidium.environments.block_world
+
+Block World environment package.
+"""
+
+__all__ =   [
+                # Environment.
+                "BlockWorld"
+            ]
+
+# Environment.
+from environments.block_world.__base__  import BlockWorld
